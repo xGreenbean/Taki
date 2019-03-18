d12da3216c800c7cce84a1c188c2a8a62d550970
Ehud Plaksin <johndoe@example.com>
2019-03-18 19:27:52 +0200

    implemented operator= and copy constructor

--- Player.cpp
+++ Player.cpp
@@ -9,9 +9,32 @@ Player::Player(std::string name, int num_of_cards){
   }
 }
 Player::~Player(){
+  clean();
+}
+void Player::clean(){
   for (size_t i = 0; i < num_of_cards; i++) {
     Card * temp = vector_cards.front();
     delete temp;
     vector_cards.pop_back();
   }
+}
+//no operator = for Card!!
+Player& Player::operator=(const Player& other){
+  if(this != &other )
+  {
+    clean();
+    this->name = other.name;
+    this->num_of_cards = other.num_of_cards;
+    for (size_t i = 0; i < num_of_cards; i++) {
+      Card * temp = new Card();
+      (*temp) = other.vector_cards.at(i);
+      vector_cards.push_back(temp);
+    }
+  }
+  return *this;
+
+}
+//might be better to implement copy & swap idiom.
+Player::Player(const Player& other){
+  (*this) = other;
 }
--- Player.h
+++ Player.h
@@ -6,10 +6,12 @@ private:
 	int num_of_cards;
 	std::vector<Card *> vector_cards;
 	//private members
-
+	void clean();
 public:
 	//public members
 	bool play(Card&);
 	~Player();
+	Player(const Player& other);
 	Player(std::string name, int num_of_cards);
+	Player& operator=(const Player& other);
 };
