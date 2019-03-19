bfe2c663793da416e9000e3baa48264f762faa92
Ehud Plaksin <johndoe@example.com>
2019-03-19 14:53:33 +0200

    on Player, added Play method

--- Player.cpp
+++ Player.cpp
@@ -12,10 +12,12 @@ Player::~Player(){
   clean();
 }
 void Player::clean(){
-  for (size_t i = 0; i < num_of_cards; i++) {
-    Card * temp = vector_cards.front();
+  auto it = vector_cards.begin();
+  while(it != vector_cards.end()){
+    Card * temp = (*it);
     delete temp;
-    vector_cards.pop_back();
+    temp = nullptr;
+    it = vector_cards.erase(it);
   }
 }
 //no operator = for Card!!
@@ -37,4 +39,52 @@ Player& Player::operator=(const Player& other){
 //might be better to implement copy & swap idiom.
 Player::Player(const Player& other){
   (*this) = other;
+}
+bool Player::play(Card& curr_card){
+  do {
+    std::cout << "current: " << curr_card << '\n';
+    std::cout << name << ", your turn -" << '\n';
+    std::cout << "Your cards: " << '\n';
+    for (size_t i = 0; i < num_of_cards; i++) {
+      std::cout << "(" << i + 1 << ")"<< (*vector_cards.at(i)) << ' ';
+    }
+    std::cout << '\n';
+
+    int choice;
+    if(getInt(choice))
+      throw std::string("Card choice ilegal");
+
+    if(choice <= 0 && choice >= num_of_cards){
+      takeCard();
+      return false;
+    }
+    choice --;
+    if(curr_card.is_legal(*vector_cards.at(choice))){
+      curr_card = (*vector_cards.at(choice));
+      putCard(choice);
+      return true;
+    }
+    std::cout << "you can't put " << (curr_card) << "on "
+    << (*vector_cards.at(choice)) << '\n';
+  } while(true);
+
+}
+void Player::takeCard(){
+  Card * temp = new Card;
+  (*temp) = Card::generate_card();
+  vector_cards.push_back(temp);
+  num_of_cards++;
+}
+void Player::putCard(int index){
+  Card * temp = vector_cards.at(index);
+  delete temp;
+  vector_cards.erase(vector_cards.begin() + index);
+  num_of_cards --;
+}
+bool Player::getInt(int& value){
+  std::cin >> value;
+  return (std::cin.fail());
+}
+bool Player::isEmpty(){
+  return num_of_cards == 0;
 }
--- Player.h
+++ Player.h
@@ -7,6 +7,9 @@ private:
 	std::vector<Card *> vector_cards;
 	//private members
 	void clean();
+	void takeCard();
+	void putCard(int index);
+	bool getInt(int& value);
 public:
 	//public members
 	bool play(Card&);
@@ -14,4 +17,5 @@ public:
 	Player(const Player& other);
 	Player(std::string name, int num_of_cards);
 	Player& operator=(const Player& other);
+	bool isEmpty();
 };
